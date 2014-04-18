3735cf140cd4b2989f3c23236cef7943f5b1bbb1
Logan Allen <loganallc@gmail.com>
2014-04-17 23:38:14 -0400

    Created a graph of recently listened to artists

--- scripts/music.js
+++ scripts/music.js
@@ -1,7 +1,7 @@
 function loadDocument() {
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
-        console.log(xhr.responseXML.documentElement.nodeName);
+        parseDocument(xhr.responseXML);
     }
     xhr.onerror = function() {
         console.log("Error while getting XML.");
@@ -11,8 +11,48 @@ function loadDocument() {
     xhr.send();
 }
 
-function parseDocument() {
+function parseDocument(xml) {   
+    var obj = {};
+    var key,
+        child,
+        xml;
+    
+    xml = xml.firstChild;
+    for (var a=0; a < xml.childElementCount; a++) {
+        child = xml.childNodes[a].nextElementSibling;
+        key = child.firstElementChild.innerHTML;
+        if(obj[key] !== obj[key]) {
+            obj[key] = parseInt(child.getElementsByTagName("playcount")[0].innerHTML);
+        } else {
+            obj[key] = obj[key] + parseInt(child.getElementsByTagName("playcount")[0].innerHTML);
+        }
+    }
+    
+    displayResults(obj);
+    
+    console.log(obj);
+}
 
+function displayResults(obj) {
+    var div,
+        p;
+    
+    for (var key in obj) {
+        if (obj[key].toString() !== "NaN") {
+            if (obj[key] > 5) {
+                div = document.createElement("div");
+                div.setAttribute("style","display:block; height:50px; width:" + obj[key]*35 + "px; background-color:#3498db");
+                p = document.createElement("p");
+                p.innerHTML = key;
+                div.appendChild(p);
+                document.body.appendChild(div);
+            }
+        }
+    }
+    
+    for (var x = 0; x < document.body.children.length; x++) {
+        console.log(element);
+    }
 }
 
 window.onload = loadDocument();

c771fe40c53de0cb3acb211f643b131ffd02e4ad
Logan Allen <loganallc@gmail.com>
2014-04-18 00:03:15 -0400

    Music elements are sorted by plays

--- scripts/music.js
+++ scripts/music.js
@@ -20,7 +20,7 @@ function parseDocument(xml) {
     xml = xml.firstChild;
     for (var a=0; a < xml.childElementCount; a++) {
         child = xml.childNodes[a].nextElementSibling;
-        key = child.firstElementChild.innerHTML;
+        key = child.getElementsByTagName("artist")[0].innerHTML;
         if(obj[key] !== obj[key]) {
             obj[key] = parseInt(child.getElementsByTagName("playcount")[0].innerHTML);
         } else {
@@ -35,19 +35,42 @@ function parseDocument(xml) {
 
 function displayResults(obj) {
     var div,
-        p;
+        p,
+        a = [];
     
     for (var key in obj) {
-        if (obj[key].toString() !== "NaN") {
-            if (obj[key] > 5) {
-                div = document.createElement("div");
-                div.setAttribute("style","display:block; height:50px; width:" + obj[key]*35 + "px; background-color:#3498db");
-                p = document.createElement("p");
-                p.innerHTML = key;
-                div.appendChild(p);
-                document.body.appendChild(div);
+        if (obj[key].toString() !== "NaN" && obj[key] > 4) {
+            a.push({"artist":key,"plays":obj[key]})
+        }
+    }
+        
+    for (var x=0; x < a.length; x++) {
+        var tPlays,
+            tArtist;
+        for (var y=x; y < a.length; y++) {
+            if (a[y].plays > a[x].plays) {
+                tPlays = a[y].plays;
+                tArtist = a[y].artist;
+                
+                a[y].plays = a[x].plays;
+                a[y].artist = a[x].artist;
+            
+                a[x].plays = tPlays;
+                a[x].artist = tArtist;
             }
         }
+    }
+    
+    console.log(a);
+    
+    for(var x=0; x < a.length; x++) {
+        console.log(a[x].artist);
+        div = document.createElement("div");
+        div.setAttribute("style","display:block; height:50px; width:" + a[x].plays*38 + "px; background-color:#3498db");
+        p = document.createElement("p");
+        p.innerHTML = a[x].artist + " &#8212; " + a[x].plays;
+        div.appendChild(p);
+        document.body.appendChild(div);
     }
     
     for (var x = 0; x < document.body.children.length; x++) {
